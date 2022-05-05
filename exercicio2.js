698b4407a018e7cfa45816d8c754ab96bfe05356
DaianaRodrigues <102737592+DaianaRodrigues@users.noreply.github.com>
2022-05-05 10:51:13 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,35 @@
+//Exercicio - Daiana Rodrigues Nascimento
+//1
+
+function nome (a) {
+    console.log(a);
+    return 
+}
+
+nome('Daiana')
+
+//2
+
+function soma (a,b) {
+    return console.log(a + b);
+}
+
+soma(9,7)
+
+//3
+
+function multiplicação (c,d) {
+    return console.log(c * d);
+}
+
+multiplicação(8,9)
+
+//4
+
+function média (a,b,c,d){
+    let res = (a + b + c + d)/4
+    console.log(res)
+    return
+}
+
+média(9,8,4,3)
